b913c1c3c4cf2fe99f7806948a95286766484e9e
giknes <biliktod8@mail.ru>
2025-05-11 21:55:13 +0300

    Hide overflow

--- src/index.jsx
+++ src/index.jsx
@@ -3,10 +3,10 @@ import { App } from "./App";
 import { createRoot } from 'react-dom/client';
 
 import { DeviceThemeProvider, SSRProvider } from '@salutejs/plasma-ui';
-import { GlobalStyle } from './GlobalStyle.tsx';
+import { GlobalStyle } from './GlobalStyle';
 
 const container = document.getElementById('root');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
+const root = createRoot(container); 
 
 root.render(
     <DeviceThemeProvider>
@@ -16,18 +16,3 @@ root.render(
         </SSRProvider>
     </DeviceThemeProvider>,
 );
-
-
-//
-// Developer Console triggers rerender in React Strict Mode while in 'development'
-//
-// More info:                                                                     
-// - https://stackoverflow.com/questions/61521734/why-does-my-create-react-app-console-log-twice
-// - https://stackoverflow.com/questions/61328285/react-component-rendering-twice-when-using-usestate-hook
-//
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
