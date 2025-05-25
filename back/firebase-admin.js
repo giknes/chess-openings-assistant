e1f78bf2e8cdbe972f58a2964acfa2da7b5ce15e
archibaldinho <147391311+arthurmisis@users.noreply.github.com>
2025-05-25 17:26:15 +0300

    Create firebase-admin.js

---
+++ back/firebase-admin.js
@@ -0,0 +1,11 @@
+// firebase-admin.js
+const admin = require("firebase-admin");
+const serviceAccount = require("./config/firebase-key.json"); // Путь к ключу
+
+// Инициализация Firebase
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore(); // Получаем доступ к Firestore
+module.exports = { db }; // Экспортируем для использования в других файлах
