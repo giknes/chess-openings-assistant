6a36d6ee623d1487036fe86dfa45a51c01adcaa0
archibaldinho <147391311+arthurmisis@users.noreply.github.com>
2025-05-26 12:34:53 +0300

    Update server.js

--- back/server.js
+++ back/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { db } = require("./back/firebase-admin");
+const { db } = require("./back/firebase-admin.js");
 const NodeCache = require("node-cache");
 const app = express();
 const cache = new NodeCache({ stdTTL: 3600 }); // Кэш на 1 час
@@ -15,8 +15,8 @@ app.get("/Openings", async (req, res) => {
       .get();
       
     const Openings = snapshot.docs.map(doc => doc.data());
-    cache.set("popular_openings", openings);
-    res.json(openings);
+    cache.set("popular_openings", Openings);
+    res.json(Openings);
   } catch (error) {
     res.status(500).send(error.message);
   }
@@ -24,8 +24,8 @@ app.get("/Openings", async (req, res) => {
 
 // Обновление популярности дебюта
 app.post("/Openings/:id/popularity", async (req, res) => {
-  const openingRef = db.collection("openings").doc(req.params.id);
-  await openingRef.update({ popularity: admin.firestore.FieldValue.increment(1) });
+  const OpeningRef = db.collection("Openings").doc(req.params.id);
+  await OpeningRef.update({ popularity: admin.firestore.FieldValue.increment(1) });
   cache.del("popular_openings"); // Сброс кэша
   res.sendStatus(200);
 });
