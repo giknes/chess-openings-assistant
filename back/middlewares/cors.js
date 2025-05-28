b866d59dfa663aa92a1c4b63328bfc242ad4d04e
archibaldinho <147391311+arthurmisis@users.noreply.github.com>
2025-05-28 20:50:18 +0300

    Update cors.js

--- back/middlewares/cors.js
+++ back/middlewares/cors.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.header("Access-Control-Allow-Origin", process.env.CORS_ORIGIN || "*");
+  res.header("Access-Control-Allow-Methods", process.env.CORS_METHODS || "GET,POST,OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 };
