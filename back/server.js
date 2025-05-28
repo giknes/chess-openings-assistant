fd33e0d57fefd38834a0c74b6b3d7a1b77fa1506
archibaldinho <147391311+arthurmisis@users.noreply.github.com>
2025-05-28 21:00:19 +0300

    Update server.js

--- back/server.js
+++ back/server.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 const express = require('express');
 const corsMiddleware = require('./middlewares/cors'); 
-const openingsRoutes = require('./routes/openings');
+const openingsRoutes = require('./routes/openingsRoutes');
 const app = express();
 
 // Middleware
